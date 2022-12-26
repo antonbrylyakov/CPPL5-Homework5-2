fbd56bb71f22dda0aa5033f4d5eb712d98e36772
Антон Брыляков <yoll@rambler.ru>
2022-12-25 23:50:17 -0800

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "table.hpp"
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	try
+	{
+		Table<int> t(2, 3);
+		t[0][0] = 4;
+		std::cout << t[0][0];
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Ошибка: " << e.what() << std::endl;
+	}
+}
---
+++ table.hpp
@@ -0,0 +1,65 @@
+﻿#pragma once
+#include <stdexcept>
+
+struct Size
+{
+	size_t width;
+	size_t height;
+};
+
+template <typename T>
+class Table
+{
+public:
+	Table(size_t width, size_t height) : m_size{ .width = width, .height = height }
+	{
+		m_values = new T * [m_size.height];
+		for (auto i = 0; i < m_size.height; ++i)
+		{
+			m_values[i] = new T[m_size.width];
+		}
+	}
+
+	Table(const Size& size) : Table(size.width, size.height)
+	{
+
+	}
+
+	// для простоты задачи запрещаем копирование
+	Table(const Table&&) = delete;
+	Table& operator=(const Table&) = delete;
+
+	~Table()
+	{
+		for (auto i = 0; i < m_size.height; i++)
+		{
+			delete[] m_values[i];
+		}
+
+		delete[] m_values;
+	}
+
+	// не возвращаем как ссылку T*&, т.к. не планируем присваивать целые строки таблицы
+	T* operator[] (size_t y)
+	{
+		checkY(y);
+		return m_values[y];
+	}
+
+	const T* operator[](size_t y) const
+	{
+		checkY(y);
+		return m_values[y];
+	}
+
+private:
+	Size m_size;
+	T** m_values;
+	void checkY(size_t y)
+	{
+		if (y > m_size.height)
+		{
+			throw std::out_of_range("Индекс строки таблицы вне диапазона");
+		}
+	}
+};
